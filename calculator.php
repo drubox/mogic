d33d1db8ba5be8e9fed695783c924b5e5de7ec97
Ignacio Sánchez <nacho@isholgueras.com>
2013-05-06 21:29:05 +0200

    Adding some styles to mogic form generator

--- calculator.php
+++ calculator.php
@@ -16,7 +16,7 @@ $margin =  $mogic->get_margin_from_form();
 $subgrids =  $mogic->get_subgrids_from_form();
 $debug =  $mogic->get_debug_from_form();
 $center =  $mogic->get_center_from_form();
-print "CENTER: $center";
+
 if ($subgrids <= 5){
   $css = $mogic->get_css();
 } else {
@@ -24,36 +24,62 @@ if ($subgrids <= 5){
 }
 ?>
 <form action="index.php" method="POST">
-  <label for="max-width"> Body max-width (px) (0 for no max-width): </label><br />
-  <input name="max-width" id="max-width" value="<?php print $max_width; ?>" /><br />
-  <label for="min-width"> Body min-width (px) (0 for no min-width): </label><br />
-  <input name="min-width" id="min-width" value="<?php print $min_width; ?>" /><br />
-  <label for="center">Center:</label><br />
-  <select name="center" id="center">
-    <option value="0" <?php print (isset($center) && $center!=1)?'selected="selected"':''; ?>>NO</option>
-    <option value="1" <?php print (isset($center) && $center==1)?'selected="selected"':''; ?>>YES</option>
-  </select><br />
-  <label for="device-width"> Device (can be 1024 for <em>g_x_1024</em>, mobile for <em>g_x_mobile</em>, m for <em>g_x_m</em>, ...): </label><br />
-  <input name="device-width" id="device-width" value="<?php print $device_width; ?>" /><br />
-  <label for="cols"> # of cols: </label><br />
-  <input name="cols" id="cols" value="<?php print $cols; ?>" /><br />
-  <label for="full-width">full width (%)</label><br />
-  <input name="full-width" id="full-width" value="<?php print $full_width; ?>" /><br />
-  <label for="margin">Margin between cols (px)</label><br />
-  <input name="margin" id="margin" value="<?php print $margin; ?>" /><br />
-  <label for="subgrids">How many subgrids (up to 5):</label><br />
-  <input name="subgrids" id="subgrids" value="<?php print $subgrids; ?>" /><br />
-  <label for="debug">Do you want debug colours?:</label><br />
-  <select name="debug" id="debug">
-    <option value="0" <?php print (isset($debug) && $debug!=1)?'selected="selected"':''; ?>>NO</option>
-    <option value="1" <?php print (isset($debug) && $debug==1)?'selected="selected"':''; ?>>YES</option>
-  </select>
-
-    <br />
-
-  Tama&ntilde;o: <?php print number_format(intval(strlen($css)) / 1024, 2) . " KB"; ?>
-  <input type="submit" value="Send" /><br />
-  <label for="css">Css (copy and paste to your file)</label><br />
-  <textarea name="css" id="css" rows="30" cols="100" readonly="readonly"><?php print $css; ?></textarea><br />
+  <div class="col1">
+    <div class="form-item">
+      <label for="max-width"> Body max-width (0 for none): </label>
+      <input name="max-width" id="max-width" value="<?php print $max_width; ?>" />
+      <span class="units">px</span>
+    </div>
+    <div class="form-item">
+    <label for="min-width"> Body min-width (0 for none): </label>
+    <input name="min-width" id="min-width" value="<?php print $min_width; ?>" />
+      <span class="units">px</span>
+    </div>
+    <div class="form-item">
+    <label for="center">Center:</label>
+    <select name="center" id="center">
+      <option value="0" <?php print (isset($center) && $center!=1)?'selected="selected"':''; ?>>NO</option>
+      <option value="1" <?php print (isset($center) && $center==1)?'selected="selected"':''; ?>>YES</option>
+    </select>
+    </div>
+    <div class="form-item">
+    <label for="device-width"> Device (can be 1024 for <em>g_x_1024</em>, mobile for <em>g_x_mobile</em>, m for <em>g_x_m</em>, ...): </label>
+    <input name="device-width" id="device-width" value="<?php print $device_width; ?>" />
+    </div>
+    <div class="form-item">
+    <label for="cols"> # of cols: </label>
+    <input name="cols" id="cols" value="<?php print $cols; ?>" />
+    </div>
+    <div class="form-item">
+    <label for="full-width">Full width:</label>
+    <input name="full-width" id="full-width" value="<?php print $full_width; ?>" />
+      <span class="units">%</span>
+    </div>
+    <div class="form-item">
+    <label for="margin">Full margin between cols:</label>
+    <input name="margin" id="margin" value="<?php print $margin; ?>" />
+      <span class="units">px</span>
+    </div>
+    <div class="form-item">
+    <label for="subgrids">How many subgrids (up to 5):</label>
+    <input name="subgrids" id="subgrids" value="<?php print $subgrids; ?>" />
+    </div>
+    <div class="form-item">
+    <label for="debug">Do you want debug colours?:</label>
+    <select name="debug" id="debug">
+      <option value="0" <?php print (isset($debug) && $debug!=1)?'selected="selected"':''; ?>>NO</option>
+      <option value="1" <?php print (isset($debug) && $debug==1)?'selected="selected"':''; ?>>YES</option>
+    </select>
+    </div>
+    <div class="form-item">
+    <input type="submit" value="Send" class="submit"/>
+    </div>
+  </div>
+  <div class="col2">
+    <div class="form-text-area">
+    <label for="css">Css (<?php print number_format(intval(strlen($css)) / 1024, 2) . " KB"; ?>):</label><br />
+    <textarea name="css" id="css" rows="30" cols="100" readonly="readonly"><?php print $css; ?></textarea>
+    </div>
+  </div>
 </form>
 
--- index.php
+++ index.php
@@ -3,42 +3,42 @@
 <!--[if IE 7]> <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
 <!--[if IE 8]> <html class="no-js lt-ie9"> <![endif]-->
 <!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <title></title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title></title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width">
 
-        <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+    <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+    <link href='http://fonts.googleapis.com/css?family=Roboto+Slab:400,300&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Noto+Sans:400,400italic&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="mogic.css">
 
-        <link rel="stylesheet" href="mogic.css">
-        <script src="js/vendor/modernizr-2.6.2.min.js"></script>
-    </head>
-    <body>
-        <!--[if lt IE 7]>
-<p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
-<![endif]-->
+    <script src="js/vendor/modernizr-2.6.2.min.js"></script>
+  </head>
+  <body>
+  <!--[if lt IE 7]>
+  <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+  <![endif]-->
 
-<h1> Mogic grid generator (In this version, only work with percentage width) </h1>
-<p>You can see <a href="demo/index.html">one demo (not responsive)</a>, <a href="demo/index2.html">another one (responsive)</a> and <a href="demo/index_animations.html">another one (animations)</a></p>
+  <h1> Mogic grid generator (In this version, only work with percentage width) </h1>
+  <p>You can see <a href="demo/index.html">one demo (not responsive)</a>, <a href="demo/index2.html">another one (responsive)</a> and <a href="demo/index_animations.html">another one (animations)</a></p>
 
-<?php include_once("calculator.php");?>
-
-<p>Note: That script has XSS by default. Enjoy hacking that :P</p>
+  <?php include_once("calculator.php");?>
 
 
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-    <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.9.1.min.js"><\/script>')</script>
-    <script src="js/plugins.js"></script>
-    <script src="js/main.js"></script>
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+  <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.9.1.min.js"><\/script>')</script>
+  <script src="js/plugins.js"></script>
+  <script src="js/main.js"></script>
 
-    <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
-    <!--script>
-      var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
-      (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
-      g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
-      s.parentNode.insertBefore(g,s)}(document,'script'));
-    </script-->
+  <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+  <!--script>
+    var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
+    (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+    g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+    s.parentNode.insertBefore(g,s)}(document,'script'));
+  </script-->
   </body>
 </html>
